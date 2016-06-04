adb101c42fdc06345af51c99710636e135637a1f
vinaypuppal <puppalvinay@gmail.com>
2016-06-04 12:50:07 +0530

    server setup

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 //Main entry point for server
+
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var app = express();
+
+//App setup
+
+
+//Server setup
+var port = process.env.PORT || 3000;
+
+var server = http.createServer(app);
+
+server.listen(port,function(error){
+	if(error) throw error;
+	console.log("server listening on port "+port);
+})
